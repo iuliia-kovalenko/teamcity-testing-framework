2b6b5dd5be63c979906ba3f898b57ffb4cc22ac1
iuliia-kovalenko <yuliakovalenko83@gmail.com>
2025-05-11 15:59:13 +0200

    Refactor groups api

--- src/main/java/com/example/teamcity/api/spec/ValidationResponseSpecifications.java
+++ src/main/java/com/example/teamcity/api/spec/ValidationResponseSpecifications.java
@@ -36,4 +36,70 @@ public class ValidationResponseSpecifications {
                                                                    formatted(projectId)));
         return responseSpecBuilder.build();
     }
+
+    public static ResponseSpecification checkProjectWithEmptyName(String projectId) {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_BAD_REQUEST);
+        responseSpecBuilder.expectBody(Matchers.containsString("Project name cannot be empty.".
+                                                                   formatted(projectId)));
+        return responseSpecBuilder.build();
+    }
+
+    public static ResponseSpecification checkProjectWithEmptyId(String projectId) {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+        responseSpecBuilder.expectBody(Matchers.containsString("Project ID must not be empty.".
+                                                                   formatted(projectId)));
+        return responseSpecBuilder.build();
+    }
+
+    public static ResponseSpecification checkProjectStartsWithNonLetterCharacter(String projectId) {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+        responseSpecBuilder.expectBody(Matchers.containsString(("Project ID \"%s\" is invalid:" +
+                                                                    " starts with non-letter character '%s'." +
+                                                                    " ID should start with a latin letter and contain only latin letters," +
+                                                                    " digits and underscores (at most 225 characters).").
+                                                                   formatted(projectId, projectId.charAt(0))));
+        return responseSpecBuilder.build();
+    }
+
+    public static ResponseSpecification checkProjectContainsOnlyLatinLettersAndUnderscores(String projectId) {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+        responseSpecBuilder.expectBody(Matchers.containsString(("Project ID \"%s\" is invalid:" +
+                                                                    " contains non-latin letter '%s'." +
+                                                                    " ID should start with a latin letter and contain only latin letters," +
+                                                                    " digits and underscores (at most 225 characters).").
+                                                                   formatted(projectId, projectId.charAt(0))));
+        return responseSpecBuilder.build();
+    }
+
+
+    public static ResponseSpecification checkProjectIdsLengthLess255(String projectId) {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+        responseSpecBuilder.expectBody(Matchers.containsString(("Project ID \"%s\" is invalid:" +
+                                                                    " it is %s characters long while the maximum length is 225. " +
+                                                                    "ID should start with a latin letter and contain only latin letters," +
+                                                                    " digits and underscores (at most 225 characters).").
+                                                                   formatted(projectId, projectId.length())));
+        return responseSpecBuilder.build();
+    }
+
+    public static ResponseSpecification checkUsersRoleCreateProject() {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_FORBIDDEN);
+        responseSpecBuilder.expectBody(Matchers.containsString("You do not have \"Create subproject\" permission in project with internal id: _Root\n" +
+                                                                    "Access denied. Check the user has enough permissions to perform the operation."));
+        return responseSpecBuilder.build();
+    }
+
+    public static ResponseSpecification checkUnauthUserCreatesProject() {
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(HttpStatus.SC_UNAUTHORIZED);
+        responseSpecBuilder.expectBody(Matchers.containsString("Incorrect username or password.\n" +
+                                                                   "To login manually go to \"/login.html\" page"));
+        return responseSpecBuilder.build();
+    }
 }
